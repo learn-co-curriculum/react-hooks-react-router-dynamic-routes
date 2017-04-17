0afb4bca6480a883a3842ac3993d9b0868cc0046
Luke Ghenco <lukeghenco@gmail.com>
2017-04-17 10:03:15 -0400

    remove unused methods form MoviesPage

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators}  from 'redux';
-import { fetchMovies } from '../actions';
 import MoviesList from '../components/MoviesList';
 
 class MoviesPage extends Component {
