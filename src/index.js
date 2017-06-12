413e2495d156e7d5e008f6b43fa9ad60c946cf2d
Luke Ghenco <lukeghenco@gmail.com>
2017-06-12 12:07:15 -0400

    Add more movies to initialState

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import App from './containers/App';
 
 const initialState = {
   movies: [
-    { id: 1, title: 'A River Runs Through It' }
+    { id: 1, title: 'A River Runs Through It' },
+    { id: 2, title: 'Se7en' },
+    { id: 3, title: 'Inception' }
   ]
 };
 
