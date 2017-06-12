3d81536ff8325f48bae235e535e439566e1b6eba
Luke Ghenco <lukeghenco@gmail.com>
2017-06-12 11:44:06 -0400

    Update MoviesPage starter code

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+// ./src/containers/MoviesPage.js
+import React from 'react';
 import { connect } from 'react-redux';
 import MoviesList from '../components/MoviesList';
 
-class MoviesPage extends Component {
-  render() {
-    return (
-      <div>
-        <MoviesList movies={this.props.movies} />
-      </div>
-    )
-  }
-}
+const MoviesPage = ({ movies }) => 
+  <div>
+    <MoviesList movies={movies} />
+  </div>;
 
 const mapStateToProps = (state) => {
   return {
